670a0161a0c406f9e768b6cfb203e398013b9944
Intencity Cloud Technologies <theintencity@gmail.com>
2025-11-30 23:00:13 -0800

    attempt to change icon when tab url changes

--- reddit-slideify/chrome-ext/background.js
+++ reddit-slideify/chrome-ext/background.js
@@ -2,6 +2,20 @@ chrome.action.onClicked.addListener((tab) => {
     chrome.tabs.sendMessage(tab.id, {type: "toggle"});
 });
 
+handler = (info) => {
+    if (info.tabId) {
+        chrome.tabs.get(info.tabId, (tab) => {
+            let match = (tab.url || "").match(/https:\/\/((.+\.reddit.com)|(xkcd.com)\/)/);
+            let icon = match ? "icon48.png" : "icon48-bw.png";
+            chrome.action.setIcon({path: icon, tabId: info.tabId});
+        });
+    }
+};
+
+chrome.tabs.onActivated.addListener(handler);
+chrome.tabs.onUpdated.addListener(handler);
+
+// this does not seem to work
 chrome.runtime.onInstalled.addListener(() => {
     chrome.action.disable();
   
